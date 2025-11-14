8b47b7d433a139962d2d000001a5df579b6046e5
Anton Makridin <makridin@gmail.com>
2025-11-14 21:09:58 +0500

    gg

--- core/utils/file_rename.py
+++ core/utils/file_rename.py
@@ -1,5 +1,9 @@
 import os
 from uuid import uuid4
+from datetime import datetime
+year = datetime.now().year
+month = datetime.now().month
+day = datetime.now().day
 
 class BookFileRenamer:
     """Класс для переименования файлов книг"""
@@ -10,7 +14,7 @@ class BookFileRenamer:
         ext = filename.split('.')[-1]  # расширение файла
         # новое имя
         new_filename = f"cover_{uuid4().hex[:8]}.{ext}"
-        return os.path.join('books/covers/', new_filename)
+        return os.path.join(f'books/covers/{year}/{month}/{day}/', new_filename)
     
     @staticmethod
     def rename_file(instance, filename):
@@ -18,4 +22,4 @@ class BookFileRenamer:
         ext = filename.split('.')[-1]
         # новое имя
         new_filename = f"book_{uuid4().hex[:8]}.{ext}"
-        return os.path.join('books/pdfs/', new_filename)
+        return os.path.join(f'books/pdfs/{year}/{month}/{day}', new_filename)
